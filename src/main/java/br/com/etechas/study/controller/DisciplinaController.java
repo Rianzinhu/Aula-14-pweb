8608a20260b64b904638464e2c24d4b1e75715d6
Rianzinhu <rianrochaaraujo1@gmail.com>
2025-05-23 19:40:10 -0300

    Aula 14-pweb

---
+++ src/main/java/br/com/etechas/study/controller/DisciplinaController.java
@@ -0,0 +1,22 @@
+package br.com.etechas.study.controller;
+
+import br.com.etechas.study.entity.Disciplina;
+
+import br.com.etechas.study.service.DisciplinaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/disciplinas")
+public class DisciplinaController {
+
+    @Autowired
+    private DisciplinaService service;
+
+    public List<Disciplina> listar(){
+        return service.listar();
+    }
+}
